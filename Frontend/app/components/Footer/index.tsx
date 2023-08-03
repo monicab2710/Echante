045305fe7add3d9f4c15aca052297e16eb821653
daisydew <d.dew.design@gmail.com>
2023-08-02 20:13:37 -0500

    template

---
+++ Frontend/app/components/Footer/index.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// MIDDLE LINKS DATA
+
+
+
+
+const footer = () => {
+    return (
+        <div className="bg-black -mt-10" id="first-section">
+            <div className="mx-auto max-w-2xl pt-8 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="mt-24 grid grid-cols-1 gap-y-10 gap-x-16 sm:grid-cols-2 lg:grid-cols-12 xl:gap-x-8">
+
+                    {/* COLUMN-1 */}
+
+                    <div className='col-span-4'>
+                        <h3 className='text-white text-4xl font-semibold leading-9 mb-4 lg:mb-20'> Enchanté</h3>
+                        <div className='flex gap-4'>
+                            <div className='footer-icons'>
+                                <Link href="https://facebook.com"><Image src={'/images/footer/vec.svg'} alt="facebook" width={15} height={20} /></Link>
+                            </div>
+                            <div className='footer-icons'>
+                                <Link href="https://twitter.com"><Image src={'/images/footer/twitter.svg'} alt="twitter" width={20} height={20} /></Link>
+                            </div>
+                            <div className='footer-icons'>
+                                <Link href="https://instagram.com"><Image src={'/images/footer/instagram.svg'} alt="instagram" width={20} height={20} /></Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* CLOUMN-2/3 */}
+
+                    
+                    
+
+                </div>
+            </div>
+
+            {/* All Rights Reserved */}
+
+            <div className="mx-auto max-w-2xl lg:max-w-7xl">
+                <div className="pt-5 pb-5 px-4 sm:px-6 lg:px-4 border-solid border-t border-footer">
+                    <div className="mt-4 grid grid-cols-1 gap-y-10 gap-x-16 sm:grid-cols-2 xl:gap-x-8">
+                        <div>
+                            <h3 className='text-center md:text-start text-offwhite text-lg'>@2023 - All Rights Reserved by <Link href="https://adminmart.com/" target="_blank"> Adminmart.com</Link></h3>
+                        </div>
+                        <div className="flex justify-center md:justify-end">
+                            <Link href="/">
+                                <h3 className="text-offwhite pr-6">Privacy policy</h3>
+                            </Link>
+                            <Link href="/">
+                                <h3 className="text-offwhite pl-6 border-solid border-l border-footer">Terms & conditions</h3>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default footer;
---
+++ Frontend/app/page.tsx
@@ -0,0 +1,16 @@
+
+import Reserve from './components/Reserve';
+
+
+
+
+
+export default function Home() {
+  return (
+    <main>
+   
+    
+      <Reserve />
+    </main>
+  )
+}
