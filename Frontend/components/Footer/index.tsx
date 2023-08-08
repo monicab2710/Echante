6e3718bebd321fda6a7c45a92315aae1bfb96dd8
daisydew <d.dew.design@gmail.com>
2023-08-07 22:17:12 -0500

    footer

--- Frontend/components/Footer/index.tsx
+++ Frontend/components/Footer/index.tsx
@@ -5,40 +5,34 @@ const Footer = () => {
   return (
     <>
       <footer
-        className="wow fadeInUp relative z-10 bg-primary bg-opacity-5 pt-16 md:pt-20 lg:pt-24"
+        className="wow fadeInUp relative z-10 bg-primary bg-opacity-5 "
         data-wow-delay=".1s"
       >
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap">
-            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
-              <div className="mb-12 max-w-[180px] lg:mb-16"></div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-primary/10 py-8">
+        <div className="bg-primary/10 py-8 text-center">
           <div className="container">
-          <div className="flex items-center">
-            <Link href="/" className="mb-1 inline-block">
-              <Image
-                src="images/logo/logo-2.svg"
-                alt="logo"
-                className="w-full dark:hidden"
-                width={70}
-                height={20}
-              />
-              <Image
-                src="images/logo/logo-2.svg"
-                alt="logo"
-                className="hidden w-full dark:block"
-                width={70}
-                height={30}
-              />
-            </Link>
+            <div className="flex flex-col items-center space-y-6">
+              <div className="flex items-center space-y-6">
+                <Link href="/" className="mb-1 inline-block">
+                  <Image
+                    src="images/logo/logo-2.svg"
+                    alt="logo"
+                    className="dark:hidden mr-10 mt-4"
+                    width={170}
+                    height={20}
+                  />
+                  <Image
+                    src="images/logo/logo-2.svg"
+                    alt="logo"
+                    className="hidden dark:block mr-10 mt-4"
+                    width={170}
+                    height={20}
+                  />
+                </Link>
             
               <a
                 href="/"
                 aria-label="social-link-fb"
-                className="mr-6 text-yellow hover:text-body-color"
+                className="mr-6 text-body-color hover:text-yellow"
               >
                 <svg
                   width="9"
@@ -52,7 +46,7 @@ const Footer = () => {
               <a
                 href="/"
                 aria-label="social-link-tw"
-                className="mr-6 text-yellow hover:text-body-color"
+                className="mr-6 text-body-color hover:text-yellow"
               >
                 <svg
                   width="19"
@@ -66,7 +60,7 @@ const Footer = () => {
               <a
                 href="/"
                 aria-label="social-link-yt"
-                className="mr-6 text-yellow hover:text-body-color"
+                className="mr-6 text-body-color hover:text-yellow"
               >
                 <svg
                   width="18"
@@ -80,7 +74,7 @@ const Footer = () => {
               <a
                 href="/"
                 aria-label="social-link-li"
-                className="mr-6 text-yellow hover:text-body-color"
+                className="mr-6 text-body-color hover:text-yellow"
               >
                 <svg
                   width="17"
@@ -91,7 +85,7 @@ const Footer = () => {
                   <path d="M15.2196 0H1.99991C1.37516 0 0.875366 0.497491 0.875366 1.11936V14.3029C0.875366 14.8999 1.37516 15.4222 1.99991 15.4222H15.1696C15.7943 15.4222 16.2941 14.9247 16.2941 14.3029V1.09448C16.3441 0.497491 15.8443 0 15.2196 0ZM5.44852 13.1089H3.17444V5.7709H5.44852V13.1089ZM4.29899 4.75104C3.54929 4.75104 2.97452 4.15405 2.97452 3.43269C2.97452 2.71133 3.57428 2.11434 4.29899 2.11434C5.02369 2.11434 5.62345 2.71133 5.62345 3.43269C5.62345 4.15405 5.07367 4.75104 4.29899 4.75104ZM14.07 13.1089H11.796V9.55183C11.796 8.7061 11.771 7.58674 10.5964 7.58674C9.39693 7.58674 9.222 8.53198 9.222 9.47721V13.1089H6.94792V5.7709H9.17202V6.79076H9.19701C9.52188 6.19377 10.2466 5.59678 11.3711 5.59678C13.6952 5.59678 14.12 7.08925 14.12 9.12897V13.1089H14.07Z" />
                 </svg>
               </a>
-
+</div>
               <p className="text-center text-base text-body-color dark:text-white">
                 © 2023 - Todos los derechos reservados. {"G2"}
               </p>
