53b364cb853c7930eecc4060bb678a432091a046
carina_jg <carinagroisman@gmail.com>
2023-08-20 19:54:11 -0300

    Agrego test con validacion de autenticacion para crear una reserva, y actualizo la collection de Postman

--- Testing/enchante-testing/src/test/java/com/enchante/enchantetesting/backend/apireservations/tests/PostReservation.java
+++ Testing/enchante-testing/src/test/java/com/enchante/enchantetesting/backend/apireservations/tests/PostReservation.java
@@ -6,6 +6,7 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.enchante.enchantetesting.extentReports.ExtentFactory;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -27,24 +28,43 @@ public class PostReservation {
     }
 
 
+    String usersURL = "http://localhost:8082/api/v1/users/auth/signin";
     String reservationsURL = "http://localhost:8087/api/v1/reservations";
 
     @Test
     public void postReservationPositive() {
+
+        JSONObject request1 = new JSONObject();
+        request1.put("email", "cfoster@mail.com");
+        request1.put("password", "cfoster_789");
+
+        String token =
+                given()
+                        .header("Content-type", "application/json")
+                        .contentType(ContentType.JSON)
+                        .body(request1.toJSONString())
+                        .when()
+                        .post(usersURL)
+                        .then().extract()
+                        .header("Authorization").toString();
+
+
         test = extent.createTest("Post de reserva Positivo");
         test.log(Status.INFO, "Inicia el test");
 
-        JSONObject request = new JSONObject();
-        request.put("time", "20:00");
-        request.put("date", "2023-09-10");
-        request.put("amountDiners", 4);
+        JSONObject request2 = new JSONObject();
+        request2.put("time", "21:00");
+        request2.put("date", "30/09/2023");
+        request2.put("amountDiners", 4);
+        request2.put("message", "message");
 
-        System.out.println(request.toJSONString());
+        System.out.println(request2.toJSONString());
 
         given()
                 .header("Content-type","application/json")
+                .header("Authorization",token)
                 .contentType(ContentType.JSON)
-                .body(request.toJSONString())
+                .body(request2.toJSONString())
                 .when()
                 .post(reservationsURL)
                 .then()
