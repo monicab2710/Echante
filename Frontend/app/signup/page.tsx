9e1d56c538f497a0e09be1eee2d550008f3d8f16
Mónica Bernal <mok2710@hotmail.com>
2023-08-19 19:39:08 -0500

    connecting with  back

--- Frontend/app/signup/page.tsx
+++ Frontend/app/signup/page.tsx
@@ -2,6 +2,16 @@
 import Link from "next/link";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axiosHelper from "../helper/axiosHelper";
+import ErrorIcon from "@mui/icons-material/Error";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+import  { useRouter } from "next/router";
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import { useState } from "react";
 
 
 const validationSchema = Yup.object({
@@ -21,13 +31,50 @@ const validationSchema = Yup.object({
 
 })
 
+const MySwal = withReactContent(Swal)
+
 const SignupPage = () => {
-  const handleSubmit = async (values, actions) => {
-    // aqui se enviaran los datos al servidor o se guardaran en la base de datos
-    alert("¡El formulario ha sido enviado con éxito!");
-    // setSubmitting(false);
-    actions.resetForm();
-  };
+// const router= useRouter(); 
+const [isRegistered, setIsRegistered] = useState(false);
+ 
+ const handleSubmit = async (values, actions) => {
+  try {
+    const response = await axiosHelper.post('/api/v1/users', {
+      name: values.name,
+      lastname: values.lastname,
+      username: values.username,
+      email: values.email,
+      password: values.password,
+    });
+
+    if (response.status === 201) {
+     
+      setIsRegistered(true);
+      MySwal.fire({
+        html: <strong>El registro se ha realizado de manera exitosa.</strong>,
+        icon: 'success',
+        showConfirmButton: false,
+        timerProgressBar: true,
+        timer: 3000,
+      });
+    } else {
+      console.log('Error:', response.data);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    MySwal.fire({
+      html: (
+        <strong>
+          Lamentablemente no ha podido registrarse. Por favor intente más tarde.
+        </strong>
+      ),
+      icon: 'warning',
+    });
+  }
+
+  actions.setSubmitting(false);
+};
+
   return (
     <>
       <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
@@ -41,12 +88,10 @@ const SignupPage = () => {
                 <p className="mb-11 text-center text-base font-medium text-body-color">
                   "¡Hazlo en segundos!"
                 </p>
-                
+
                 <div className="mb-8 flex items-center justify-center">
-                  
+
                 </div>
-
-
                 <Formik
                   initialValues={{
                     name: "",
@@ -216,13 +261,13 @@ const SignupPage = () => {
                                   <path
                                     d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
                                     className="stroke-width-[0.4] fill-[#0D263B] stroke-[#0D263B] dark:fill-[#EA7363] dark:stroke-[#EA7363]"
-                                />
+                                  />
                                 </svg>
                               </span>
                             </div>
                           </div>
                           <span>
-                          Al crear una cuenta significa que aceptas los
+                            Al crear una cuenta significa que aceptas los
                             <a
                               href="#0"
                               className="text-primary dark:text-white hover:underline"
