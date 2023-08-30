705e837e37e7dc7536e4bd4a6d7469f14ce7bcee
belldaiana <belcita814@gmail.com>
2023-08-30 15:56:56 -0300

    Subiendo cambio de tabla user para token

--- Base de datos/user.sql
+++ Base de datos/user.sql
@@ -56,6 +56,7 @@ CREATE TABLE `user` (
   `user_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
   `role_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_user_rol_idx` (`role_id`),
@@ -81,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-19 19:55:28
+-- Dump completed on 2023-08-30 15:55:04
