2d61621844618459a772037fddc5a65719d9cbed
Monica Bernal <mok2710@hotmail.com>
2023-09-03 22:22:59 -0500

    feat: handle change user profile inputs

--- Frontend/app/signin/page.tsx
+++ Frontend/app/signin/page.tsx
@@ -9,6 +9,7 @@ import * as Yup from "yup";
 import { useRouter } from 'next/navigation'
 import jwt_decode from "jwt-decode";
 import { UserContext } from "../providers";
+import { decoded } from '@/app/helper/global'
 
 interface DecodedData {
   name: string;
@@ -83,16 +84,10 @@ const SigninPage = () => {
         const bearerToken = res.data.token
         const token = bearerToken.split(" ")[1]
         sessionStorage.setItem('token', token);
-        const decoded = jwt_decode(token);
-        console.log("decode", decoded)
-        const userStorage = JSON.stringify({
-          name: decoded.name,
-          lastName: decoded.lastName,
-          email: decoded.sub,
-          userId: decoded.userId
-        })
+        const profile = decoded(token);
+        const userStorage = JSON.stringify(profile)
         sessionStorage.setItem('user', userStorage);
-        setUser(userStorage);
+        setUser(profile);
         Toast.fire({
           icon: 'success',
           title: 'Inicio de sesión exitoso.'
---
+++ Frontend/types/profile.ts
@@ -0,0 +1,19 @@
+export type Profile = {
+  name: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  userId: number;
+};
+
+
+export type User = {
+  sub: string;
+  userId: number;
+  name: string;
+  lastName: string;
+  userName: string;
+  iat: number;
+  exp: number;
+};
+
