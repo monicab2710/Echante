7d44b22e1f7ebb58cbcdb6598c5faf184ba10dd1
carina_jg <carinagroisman@gmail.com>
2023-09-07 00:05:59 -0300

    Agrego tests para random products

---
+++ Testing/backend/enchante-testing/src/test/java/com/enchante/enchantetesting/backend/apiproducts/tests/GetRandomProducts.java
@@ -0,0 +1,51 @@
+package com.enchante.enchantetesting.backend.apiproducts.tests;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.enchante.enchantetesting.extentReports.ExtentFactory;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import static io.restassured.RestAssured.get;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GetRandomProducts {
+
+    static ExtentSparkReporter spark = new ExtentSparkReporter("src/test/java/com/enchante/enchantetesting/backend/apiproducts/reports/getRandomProducts.html");
+    static ExtentReports extent;
+    ExtentTest test;
+
+    @BeforeAll
+    public void setUp() {
+        extent = ExtentFactory.getInstance();
+        extent.attachReporter(spark);
+    }
+
+
+    String productsURL = "http://localhost:8081/api/v1/products/random";
+    Response response = get(productsURL);
+
+    @Test
+    @Tag("Smoke")
+    public void getRandomProducts() {
+        test = extent.createTest("Get random de productos Positivo");
+        test.log(Status.INFO, "Inicia el test");
+
+        int statusCode = response.getStatusCode();
+        System.out.println("Response status is: " + statusCode);
+
+        response
+                .then()
+                .assertThat().statusCode(200)
+                .and().log().all();
+
+        test.log(Status.PASS, "Validación del código de estado 200");
+        test.log(Status.INFO, "Finaliza el test");
+    }
+
+    @AfterAll
+    public void quit() {
+        extent.flush();
+    }
+}
