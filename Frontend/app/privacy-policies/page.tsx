29ea1eff8e6735163e1066e74e25ecc4d42af4aa
pipeparra <pipeparra32@gmail.com>
2023-09-13 02:50:24 -0500

    politicasyCondiciones

---
+++ Frontend/app/privacy-policies/page.tsx
@@ -0,0 +1,12 @@
+
+import PrivacyPage from "@/components/Privacy-Policies/privacy-policies";
+
+const PrivacyPoli = () => {
+    return (
+      <>
+        < PrivacyPage/>
+      </>
+    );
+  };
+  
+  export default PrivacyPoli;
