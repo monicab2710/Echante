44ea5f2b0d174ca4d5188fc94c1d7642f3aed75d
daisydew <d.dew.design@gmail.com>
2023-09-13 22:00:31 -0500

    categorias responsivas 'pendiente plato ppal'

--- Frontend/components/Hero/HeroSectionCategories.tsx
+++ Frontend/components/Hero/HeroSectionCategories.tsx
@@ -20,48 +20,46 @@ const HeroSectionCategories = () => {
   }, []);
 
   const renderCategories = ({ id, title, imageUrl }) => (
-    <div className="2xl:w-full xl:w-full sm:w-1/2 md:w-1/2 lg:w-1/4" key={id}>
-      <div className="relative w-full h-64">
-        <Image src={imageUrl} layout="fill" objectFit="cover" alt={title} />
-        <div className="absolute bottom-0 left-0 w-full p-1 bg-white bg-opacity-70 text-white text-center">
-          <h4 className="mb-2 text-sm sm:text-xl font-medium text-black">{title}</h4>
+    <div className="2xl:w-full xl:w-full lg:w-full sm:w-full md:w-full lg:w-1/4" key={id}>
+      <div className="relative w-full ">
+        <Image src={imageUrl} layout="responsive" width={16} height={9} objectFit="cover" alt={title} />
+        <div className="absolute bottom-0 left-0 aspect-ratio-16/9 w-full p-1 bg-white bg-opacity-70 text-white text-center">
+          <h4 className="my-2 text-xl sm:text-xl font-medium text-black">{title}</h4>
         </div>
       </div>
     </div>
   );
 
   return (
-    <section className="pt-16 md:pt-20 lg:pt-28 px-4 md:px-8 lg:px-16 xl:px-32 relative">
-  <div className="flex flex-row flex-col" style={{ zIndex: 0 }}>
-    <div className="w-full h-0 opacity-30" style={{ zIndex: 0 }}>
-      <Image
-        src="/images/dessert.jpg"
-        layout="fill"
-        objectFit="cover"
-        alt="Imagen"
-      />
-    </div>
-    <div className=" md:w-1/2 2xl:w-full xl:w-full mt-8 md:mt-0" style={{ zIndex: 1 }}>
-      <h1 className="2xl:px-10 text-center 2xl:text-3xl 2xl:mb-10 xl:mb-10 xl:text-3xl md:text-3xl lg:text-2xl font-bold text-white">Bienvenido a Enchanté</h1>
-      <p className="2xl:w-1/2 2xl:mx-auto 2xl:text-lg xl:w-full xl:text-lg xl:mb-12 text-center  text-sm md:text-base lg:text-sm font-medium text-black dark:text-yellow" >
-        ¡Bienvenidos a nuestro encantador restaurante francés! Sumérgete en
-        una experiencia culinaria única que te transportará directamente a
-        las calles empedradas de París. Nuestro restaurante está dedicado a
-        brindarte los auténticos sabores y delicias de la gastronomía francesa.
-        Desde exquisitos platos de alta cocina hasta clásicos reconfortantes,
-        cada bocado es una obra maestra culinaria cuidadosamente preparada
-        por nuestros talentosos chefs. Explora nuestras cuatro
-          encantadoras categorías a continuación y déjate llevar
-          por una experiencia gastronómica de la cocina francesa tradicional y contemporánea.
-      </p>
-    </div>
-  </div>
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 2xl:pb-10 xl:pb-10 xl:pt-70">
-    {categories.map(renderCategories)}
-  </div>
-</section>
-
+    <section className="pt-20 lg:pt-28 px-4 md:px-8 lg:px-16 xl:px-32 relative">
+      <div className="flex flex-row flex-col">
+        <div className="w-full h-0 opacity-30" style={{ zIndex: 0 }}>
+          <Image
+            src="/images/macahero.jpg"
+            layout="fill"
+            objectFit="cover"
+            alt="Imagen"
+          />
+        </div>
+        <div className="2xl:w-full xl:w-full mt-8 md:mt-0" style={{ zIndex: 1 }}>
+          <h1 className="2xl:px-10 text-center 2xl:text-3xl xl:mb-10 xl:text-3xl md:text-2xl sm:text-2xl xs:text-2xl xs:mb-4 sm:mb-4 md:mt-10 md:mb-5 lg:text-3xl font-bold text-white">Bienvenido a Enchanté</h1>
+          <p className="2xl:w-2/3 2xl:mx-auto 2xl:text-lg xl:w-2/3 xl:mx-auto xl:text-lg lg:w-full 2xl:mx-auto text-center text-sm md:text-base lg:text-lg font-medium text-black dark:text-yellow sm:hidden">
+            Sumérgete en una experiencia culinaria única que te transportará directamente a las calles empedradas de París. Nuestro restaurante está dedicado a brindarte los auténticos sabores y delicias de la gastronomía francesa.
+          </p>
+          <p className="2xl:w-3/4 2xl:mx-auto 2xl:text-lg xl:w-2/3 xl:mx-auto xl:text-lg lg:w-full 2xl:mx-auto text-center text-sm md:text-base lg:text-lg font-medium text-black dark:text-yellow hidden sm:block">
+            Sumérgete en una experiencia culinaria única que te transportará directamente a las calles empedradas de París. Nuestro restaurante está dedicado a
+            brindarte los auténticos sabores y delicias de la gastronomía francesa.
+            Desde exquisitos platos de alta cocina hasta clásicos reconfortantes, cada bocado es una obra maestra culinaria cuidadosamente preparada
+            por nuestros talentosos chefs. Explora nuestras cuatro encantadoras categorías a continuación y déjate llevar por una experiencia gastronómica de la cocina francesa tradicional y contemporánea.
+          </p>
+        </div>
+      </div>
+      <div className="grid 2xl:grid-cols-4 2xl:pb-10 2xl:pt-8 xl:pt-5 xl:grid-cols-4 sm:grid-cols-2 md:grid-cols-2 gap-4 xl:pb-10 md:pb-10 md:pt-7 sm:pb-8 sm:pt-4 xs:pb-4 xs:pt-4 lg:pb-10 lg:pt-10">
+        {categories.map(renderCategories)}
+      </div>
+    </section>
   );
 };
 
 export default HeroSectionCategories;
+
