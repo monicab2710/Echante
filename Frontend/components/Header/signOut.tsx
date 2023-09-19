aad29f22ac48ada81ff313da503d1dd3999b3750
Mónica Bernal <mok2710@hotmail.com>
2023-09-19 23:14:17 +0000

    updating admin page

--- Frontend/components/Header/signOut.tsx
+++ Frontend/components/Header/signOut.tsx
@@ -6,14 +6,14 @@ import { useContext } from 'react';
 
 
 
-export default function SignOut({currentUser}) {
-  const router = useRouter();
+export default function SignOut({ currentUser }) {
+    const router = useRouter();
 
-  let { user, setUser } = useContext(UserContext)
-      const removeUser = () => {
+    let { user, setUser } = useContext(UserContext)
+    const removeUser = () => {
         sessionStorage.removeItem("user");
         sessionStorage.removeItem("token");
-        router.push('/')    
+        router.push('/')
         setUser(null);
     }
     return (
@@ -22,11 +22,12 @@ export default function SignOut({currentUser}) {
             <p
                 className=" py-3 px-7 text-base font-bold text-black dark:text-yellow dark:hover:text-body-color  md:block"
             >
-               !Hola! {user.name} {user.lastName}
-              
+
+                ¡Hola! {user.name} {user.lastName}
+
             </p>
-            <div className="mb-6">
-                <button onClick={removeUser} className="flex w-full items-center justify-center rounded-md bg-white px-9 py-4 text-base font-semibold text-primary transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+            <div className="mb-4 mb-md-0">
+                <button onClick={removeUser} className="py-3 px-7 text-base font-bold text-black dark:text-yellow dark:hover:text-body-color  md:block">
                     Cerrar sesión
                 </button>
             </div>
