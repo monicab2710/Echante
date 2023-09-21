1a92c549c4e57f1938d26667c964604607951207
pipeparra <pipeparra32@gmail.com>
2023-09-20 21:06:46 -0500

    Defectos, terminos,  politicas y readme

--- Frontend/components/Terms-Conditions/terms-conditions.tsx
+++ Frontend/components/Terms-Conditions/terms-conditions.tsx
@@ -6,19 +6,10 @@ const TermsConditions = () => {
       <div className="container">
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
-            <div
-              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
-              <Image
-                src="/images/about/resta.webp"
-                alt="foto"
-                fill
-              />
-            </div>
+            
           </div>
-          <div className="w-full px-4 lg:w-1/2">
-            <div className="wow fadeInUp max-w-[470px]" data-wow-delay=".2s">
+          <div className="w-full px-4 ">
+            <div className="wow fadeInUp max-w-[1200px]" data-wow-delay=".2s">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                 ¡Terminos y Condiciones Enchanté!
